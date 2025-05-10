0400b8392a5f2d6c9f79dc7ec62a19fef343494e
Gabriel Cabral <gabrielcabral@MacBook-Pro-de-Gabriel.local>
2025-05-10 19:36:44 -0300

    feat: primeiro commit

---
+++ src/main/java/gabriel/dev/spring_boot_crud_java/SpringBootCrudJavaApplication.java
@@ -0,0 +1,13 @@
+package gabriel.dev.spring_boot_crud_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabriel/dev/spring_boot_crud_java/SpringBootCrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.dev.spring_boot_crud_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
