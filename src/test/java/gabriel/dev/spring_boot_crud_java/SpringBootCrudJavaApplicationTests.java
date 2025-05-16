ead15e72d30cb8f715e4bbce3bf742dad6fbefd2
Gabriel Cabral <gabrielcabral@MacBook-Pro-de-Gabriel.local>
2025-05-16 20:50:40 -0300

    Feat: testes automatizados

--- src/test/java/gabriel/dev/spring_boot_crud_java/SpringBootCrudJavaApplicationTests.java
+++ src/test/java/gabriel/dev/spring_boot_crud_java/SpringBootCrudJavaApplicationTests.java
@@ -2,7 +2,7 @@ package gabriel.dev.spring_boot_crud_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class SpringBootCrudJavaApplicationTests {
 
@@ -11,3 +11,5 @@ class SpringBootCrudJavaApplicationTests {
 	}
 
 }
+
+ */
